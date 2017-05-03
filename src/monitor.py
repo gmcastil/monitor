f4b0cde6ec0b4ddc8a074ec5a20e00f1a01509bd
George Castillo <gmcastil@gmail.com>
2017-05-02 18:41:53 -0600

    Added the ability to walk directories using a generator.

--- src/monitor.py
+++ src/monitor.py
@@ -1,18 +1,25 @@
+#!/usr/bin/python3
+
 import os
+import sys
 
 import curses
 from curses import wrapper
 
 def main(stdscr):
 
-    stdscr.refresh()
-    stdscr.getkey()
+    files = abs_files(sys.argv[1])
 
-def filename_gen(top_dir):
-    for dirpath, _, filenames in os.walk(top_dir):
+    # stdscr.refresh()
+    # stdscr.getkey()
+    print(sys.argv[1])
+
+def abs_files(top_dir):
+    for dirpath, dirnames, filenames in os.walk(top_dir):
         for f in filenames:
             yield os.path.abspath(os.path.join(dirpath, f))
 
 if __name__ == "__main__":
+    print(sys.argv[1])
     stdscr = curses.initscr()
     wrapper(main)
